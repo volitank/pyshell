5e05087d559d9bfb7fb2f7afba9d766ff66af0a4
volitank <lee.blake.a@gmail.com>
2021-11-28 16:30:42 -0500

    Small changes for build
    Pyshell would not build and I'm not sure why. But removing the import from setup.py worked.

--- pyshell/__init__.py
+++ pyshell/__init__.py
@@ -1,10 +1,4 @@
-
-__title__ = 'pyshell'
-__author__ = 'volitank'
-__license__ = 'GPLv3'
-__copyright__ = '2021 volitank'
-__version__ = '1.0.0a6'
-
+# Import the class to top level
 from .pyshell import pyshell
 
 # Define a basic shell to import quick and dirty command spam
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # Always prefer setuptools over distutils
 from setuptools import setup
 from pathlib import Path
-import pyshell
 # Define the directory that setup.py is in
 here = Path(__file__).parent.resolve()
 
@@ -12,13 +11,13 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    name=pyshell.__title__,  # Required
-    version=pyshell.__version__,  # Required
+    name='pyshell',  # Required
+    version='1.0.0a6',  # Required
     description='A Linux subprocess module.',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
     url='https://github.com/volitank/pyshell',  # Optional
-    author=pyshell.__author__,  # Optional
+    author='volitank',  # Optional
     author_email='blake@volitank.com',  # Optional
     classifiers=[  # Optional
 	# List of classifiers https://gist.github.com/nazrulworld/3800c84e28dc464b2b30cec8bc1287fc
@@ -42,7 +41,7 @@ setup(
     keywords='python, shell, subprocess',  # Optional
     packages=['pyshell'],  # Required
     python_requires='>=3.6, <4',
-
+    install_requires=['pexpect'],
     project_urls={  # Optional
         'Documentation': 'https://volitank.com/pyshell',
         'Source': 'https://github.com/volitank/pyshell',
